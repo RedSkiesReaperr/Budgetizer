9a32611183c6d845d04b493e844383408a96fdae
RedSkiesReaperr <theopouletsouchard@gmail.com>
2023-08-02 11:41:51 +0200

    Fix CSV reader enconding

--- api/app/lib/file_readers/csv.rb
+++ api/app/lib/file_readers/csv.rb
@@ -7,7 +7,7 @@ module FileReaders
     module_function
 
     def read(file_path:, separator: ';')
-      file = File.read(file_path)
+      file = File.read(file_path, encoding: 'bom|utf-8')
       csv = CSV.parse(file, headers: true, col_sep: separator)
 
       csv.each do |line|
