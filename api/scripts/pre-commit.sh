50afc36356ba989b2381eeb79a2b5c1b316e9081
RedSkiesReaperr <theopouletsouchard@gmail.com>
2023-08-03 15:21:13 +0200

    Fix pre-commit failure case

--- api/scripts/pre-commit.sh
+++ api/scripts/pre-commit.sh
@@ -4,11 +4,20 @@ echo "Initializing rbenv"
 eval "$(rbenv init -)"
 
 echo "Running pre-push hook"
-./scripts/run-rubocop.sh
-./scripts/run-brakeman.sh
-./scripts/run-specs.sh
 
-# $? stores exit value of the last command
+./scripts/run-rubocop.sh
+if [ $? -ne 0 ]; then
+ echo "Rubocop must pass before pushing!"
+ exit 1
+fi
+
+./scripts/run-brakeman.sh
+if [ $? -ne 0 ]; then
+ echo "Rubocop & Brakeman must pass before pushing!"
+ exit 1
+fi
+
+./scripts/run-specs.sh
 if [ $? -ne 0 ]; then
  echo "Rubocop, Brakeman & Specs must pass before pushing!"
  exit 1
