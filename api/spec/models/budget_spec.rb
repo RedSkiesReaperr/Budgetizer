a5c08170e47b996d97a316ff928d5a3094a724a6
RedSkiesReaperr <theopouletsouchard@gmail.com>
2023-08-03 12:28:27 +0200

    Add spec for new Budget model methods

--- api/spec/models/budget_spec.rb
+++ api/spec/models/budget_spec.rb
@@ -35,4 +35,54 @@ describe Budget do
       expect(budget.forecast_saving).to eq(expected)
     end
   end
+
+  describe 'forecast_vital_diff' do
+    it do
+      expected = budget.forecast_vital - budget.objective.vital_value
+
+      expect(budget.forecast_vital_diff).to eq(expected)
+    end
+  end
+
+  describe 'forecast_non_essential_diff' do
+    it do
+      expected = budget.forecast_non_essential - budget.objective.non_essential_value
+
+      expect(budget.forecast_non_essential_diff).to eq(expected)
+    end
+  end
+
+  describe 'forecast_saving_diff' do
+    it do
+      expected = budget.forecast_vital - budget.objective.saving_value
+
+      expect(budget.forecast_saving_diff).to eq(expected)
+    end
+  end
+
+  describe 'forecast_vital_diff_percentage' do
+    it do
+      expected = ((budget.forecast_vital - budget.objective.vital_value) / budget.objective.vital_value) * 100
+
+      expect(budget.forecast_vital_diff_percentage).to eq(expected)
+    end
+  end
+
+  describe 'forecast_non_essential_diff_percentage' do
+    it do
+      expected = budget.forecast_non_essential - budget.objective.non_essential_value
+      expected /= budget.objective.non_essential_value
+      expected *= 100
+
+      expect(budget.forecast_non_essential_diff_percentage).to eq(expected)
+    end
+  end
+
+  describe 'forecast_saving_diff_percentage' do
+    it do
+      expected = ((budget.forecast_saving - budget.objective.saving_value) / budget.objective.saving_value) * 100
+
+      expect(budget.forecast_saving_diff_percentage).to eq(expected)
+    end
+  end
 end
