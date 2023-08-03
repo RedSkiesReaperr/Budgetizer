5c68e1ad7bbd25cefba46477eb47fda31f6a91c1
RedSkiesReaperr <theopouletsouchard@gmail.com>
2023-08-03 12:53:38 +0200

    Add Objective percentages validation

--- api/app/models/objective.rb
+++ api/app/models/objective.rb
@@ -5,6 +5,8 @@ class Objective < ApplicationRecord
 
   validates :vital, :non_essential, :saving, presence: true, numericality: { greater_than: 0, less_than: 100 }
 
+  validate :sum_is_one_hundred
+
   def vital_value
     vital * budget.forecast_income / 100
   end
@@ -15,5 +17,15 @@ class Objective < ApplicationRecord
 
   def saving_value
     saving * budget.forecast_income / 100
+  end
+
+  private
+
+  def sum_is_one_hundred
+    sum = vital + non_essential + saving
+
+    return if sum == 100
+
+    errors.add(:attributes, "Sum of vital, non_essential & saving should equal 100. Got: '#{sum}'")
   end
 end
--- api/spec/models/objective_spec.rb
+++ api/spec/models/objective_spec.rb
@@ -20,4 +20,30 @@ describe Objective do
   describe '.saving_value' do
     it { expect(objective.saving_value).to eq(640.3739999999999) }
   end
+
+  describe 'sum_is_one_hundred validation' do
+    context 'when sum of objective fields are not equal to 100' do
+      let(:vital) { 99 }
+      let(:non_essential) { 10 }
+      let(:saving) { 10 }
+
+      it {
+        expect do
+          described_class.create!(vital:, non_essential:, saving:)
+        end.to raise_error(ActiveRecord::RecordInvalid)
+      }
+    end
+
+    context 'when sum of objective fields are equal to 100' do
+      let(:vital) { 70 }
+      let(:non_essential) { 15 }
+      let(:saving) { 15 }
+
+      it {
+        expect do
+          described_class.create!(vital:, non_essential:, saving:)
+        end.not_to raise_error(ActiveRecord::RecordInvalid)
+      }
+    end
+  end
 end
