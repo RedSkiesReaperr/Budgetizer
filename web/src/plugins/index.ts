e56071bef807c0a2adb445ae1bf50915e4d72b58
RedSkiesReaperr <theopouletsouchard@gmail.com>
2023-08-07 10:13:20 +0200

    Remove default useless code

--- web/src/plugins/index.ts
+++ web/src/plugins/index.ts
@@ -5,15 +5,15 @@
  */
 
 // Plugins
-import { loadFonts } from './webfontloader'
+import {loadFonts} from './webfontloader'
 import vuetify from './vuetify'
 import pinia from '../store'
 import router from '../router'
 
 // Types
-import type { App } from 'vue'
+import type {App} from 'vue'
 
-export function registerPlugins (app: App) {
+export function registerPlugins(app: App) {
   loadFonts()
   app
     .use(vuetify)
