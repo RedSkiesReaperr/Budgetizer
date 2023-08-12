8ad03a82015120a1f0af1a149e681399cb2450ca
RedSkiesReaperr <theopouletsouchard@gmail.com>
2023-08-12 15:20:36 +0200

    Fill earnings vue

--- web/src/views/budget/Earnings.vue
+++ web/src/views/budget/Earnings.vue
@@ -1,9 +1,32 @@
-<script lang="ts" setup>
-</script>
-
 <template>
-  Earnings
+  <BasicCard :loading="operationsStore.incomes.length <= 0">
+    <OperationsTable :data="operationsStore.incomes" />
+  </BasicCard>
 </template>
 
 <script lang="ts">
+import { Operation } from "@/api/resources/operations";
+import { useOperationsStore } from "@/stores/operations";
+import BasicCard from "@/components/BasicCard.vue";
+import OperationsTable from "@/components/OperationsTable.vue";
+
+export default {
+  setup() {
+    const operationsStore = useOperationsStore();
+
+    return { operationsStore };
+  },
+  data() {
+    return {
+      incomesLoading: true,
+      operations: [] as Array<Operation>,
+    };
+  },
+  created() {
+    if (this.operationsStore.incomes.length <= 0) {
+      this.operationsStore.fetchAll();
+    }
+  },
+  components: { BasicCard, OperationsTable },
+};
 </script>
