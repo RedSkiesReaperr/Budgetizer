2840a5ee656dd1ea302bd864ba7145b999ce46a0
RedSkiesReaperr <theopouletsouchard@gmail.com>
2023-11-01 16:46:26 +0100

    Add calculations.sum<T> function

--- web/src/services/calculations.test.ts
+++ web/src/services/calculations.test.ts
@@ -1,9 +1,11 @@
-import { describe, expect, it } from "vitest";
+import {describe, expect, it} from "vitest";
 import {
-  percentageToValue,
+  percentageToValue, sum,
   valueToPercentage,
   variationPercentage,
 } from "./calculations";
+import {Operation} from "@/api/resources/operations";
+import {Line} from "@/api/resources/lines";
 
 describe("Calculations service", () => {
   describe("percentageToValue()", () => {
@@ -22,5 +24,30 @@ describe("Calculations service", () => {
     it("returns correct variation", () => {
       expect(variationPercentage(65, 785)).toEqual(1107.6923076923076);
     });
+  });
+
+  describe("sum()", () => {
+    describe("sum<Operation>()", () => {
+      const operations: Operation[] = [
+        {attributes: {amount: 289.33, date: "", label: "", pointed: true, opType: "", category: "", comment: ""}, id: "", type: "", links: {self: ""}},
+        {attributes: {amount: 98.13, date: "", label: "", pointed: true, opType: "", category: "", comment: ""}, id: "", type: "", links: {self: ""}},
+        {attributes: {amount: 3.92, date: "", label: "", pointed: true, opType: "", category: "", comment: ""}, id: "", type: "", links: {self: ""}},
+      ]
+      it("returns correct sum", () => {
+        expect(sum(operations)).toEqual(391.38);
+      });
+    })
+
+    describe("sum<Line>()", () => {
+      const lines: Line[] = [
+        {attributes: {amount: 982.73, label: "", category: "", lineType: ""}, id: "", links: {self: ""}, type: "", relationships: {budget: {links: {self: "", related: ""}}}},
+        {attributes: {amount: 273.02, label: "", category: "", lineType: ""}, id: "", links: {self: ""}, type: "", relationships: {budget: {links: {self: "", related: ""}}}},
+        {attributes: {amount: 94.87, label: "", category: "", lineType: ""}, id: "", links: {self: ""}, type: "", relationships: {budget: {links: {self: "", related: ""}}}},
+      ]
+
+      it("returns correct sum", () => {
+        expect(sum(lines)).toEqual(1350.62);
+      });
+    })
   });
 });
--- web/src/services/calculations.ts
+++ web/src/services/calculations.ts
@@ -1,3 +1,6 @@
+import {Operation} from "@/api/resources/operations";
+import {Line} from "@/api/resources/lines";
+
 export function percentageToValue(percentage: number, value: number): number {
   return (percentage * value) / 100;
 }
@@ -8,4 +11,8 @@ export function valueToPercentage(value: number, total: number): number {
 
 export function variationPercentage(v1: number, v2: number): number {
   return ((v2 - v1) / v1) * 100;
+}
+
+export function sum<T extends Operation | Line>(members: T[]): number {
+  return members.reduce((sum: number, m: T) => sum + m.attributes.amount, 0)
 }
