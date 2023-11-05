3ee93c4500a2ac6ff6e7d7a10f910eb4b38231d1
RedSkiesReaperr <theopouletsouchard@gmail.com>
2023-11-05 16:52:55 +0100

    Move git hooks to root

--- git-hooks/pre-commit.sh
+++ git-hooks/pre-commit.sh
@@ -5,19 +5,19 @@ eval "$(rbenv init -)"
 
 echo "Running pre-push hook"
 
-./scripts/run-rubocop.sh
+./run-rubocop.sh
 if [ $? -ne 0 ]; then
  echo "Rubocop must pass before pushing!"
  exit 1
 fi
 
-./scripts/run-brakeman.sh
+./run-brakeman.sh
 if [ $? -ne 0 ]; then
  echo "Rubocop & Brakeman must pass before pushing!"
  exit 1
 fi
 
-./scripts/run-specs.sh
+./run-specs.sh
 if [ $? -ne 0 ]; then
  echo "Rubocop, Brakeman & Specs must pass before pushing!"
  exit 1
