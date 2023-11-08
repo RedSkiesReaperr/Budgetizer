c18004bb2fcc2ffea8f2f807683c270acf660db4
RedSkiesReaperr <theopouletsouchard@gmail.com>
2023-11-08 09:21:55 +0100

    Randomize db/seeds data

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -8,9 +8,58 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create!(provider: 'email',
-             uid: 'john.doe@gmail.com',
-             password: '123azerty',
-             name: 'John',
-             nickname: 'John.Doe',
-             email: 'john.doe@gmail.com')
+NUMBER_OF_BUDGETS = Faker::Number.within(range: 0..10)
+NUMBER_OF_LINES_PER_BUDGET = Faker::Number.within(range: 30..60)
+NUMBER_OF_OPERATIONS = Faker::Number.within(range: 1000..10_000)
+
+User.create(provider: 'email',
+            uid: 'john.doe@gmail.com',
+            password: '123azerty',
+            name: 'John',
+            nickname: 'John.Doe',
+            email: 'john.doe@gmail.com')
+
+NUMBER_OF_BUDGETS.times do
+  variation = Faker::Number.within(range: 0..20)
+  vital = 50 - (variation / Faker::Number.within(range: 1..10))
+  non_essential = 30 + (variation / Faker::Number.within(range: 1..5))
+  saving = 100 - vital - non_essential
+
+  objective = Objective.create!(vital:, non_essential:, saving:)
+  budget = Budget.create(objective:, name: "#{Faker::Adjective.positive} budget")
+
+  NUMBER_OF_LINES_PER_BUDGET.times do
+    amount = Faker::Number.within(range: -500.0..500.0)
+    category = Faker::Number.within(range: 0...Line.categories.size)
+    line_type = if amount.positive?
+                  :income
+                else
+                  Faker::Boolean.boolean ? :vital : :non_essential
+                end
+    Line.create(label: Faker::Hobby.activity, amount:, line_type:, budget:, category:)
+  end
+end
+
+NUMBER_OF_OPERATIONS.times do
+  date = Faker::Date.between(from: 6.months.ago, to: 1.day.ago)
+  label = "#{Faker::Invoice.reference} #{Faker::Company.name}"
+  amount = Faker::Number.within(range: -500.0..500.0)
+  comment = Faker::Boolean.boolean ? Faker::ChuckNorris.fact : nil
+  pointed = Faker::Boolean.boolean
+  op_type = if amount.positive?
+              :income
+            else
+              Faker::Number.within(range: 1...Operation.op_types.size)
+            end
+
+  Operation.create(
+    identifier: SecureRandom.uuid,
+    category: Faker::Number.within(range: 0...Operation.categories.size),
+    date:,
+    label:,
+    amount:,
+    comment:,
+    pointed:,
+    op_type:
+  )
+end
