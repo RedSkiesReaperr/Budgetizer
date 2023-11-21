9f5f049a4acf958cc0249380862e203e78607016
RedSkiesReaperr <theopouletsouchard@gmail.com>
2023-11-21 10:36:16 +0100

    Fix services/operations tests

--- web/src/services/operations.test.ts
+++ web/src/services/operations.test.ts
@@ -1,9 +1,7 @@
 import {describe, expect, it} from "vitest";
 import {copyOperation, operationsForCategories, operationsForDay} from "@/services/operations";
 import {Operation} from "@/api/resources/operations";
-import {Line} from "@/api/resources/lines";
-import {linesForTypes} from "@/services/lines";
-import moment from "moment";
+import moment from "moment/moment";
 
 describe("Operations service", () => {
   describe("copyOperation()", () => {
@@ -140,23 +138,22 @@ describe("Operations service", () => {
       {
         attributes: {
           amount: 289.33,
-          date: "2023-08-30T00:00:00.000Z",
+          date: "2023-08-31T00:00:00.000Z",
           label: "",
           pointed: true,
           opType: "",
-          category: "CategoryA",
+          category: "CategoryB",
           comment: ""
         },
         id: "",
         type: "",
         links: {self: ""}
-      },
+      }
     ]
 
     it("returns a real copy", () => {
-      const date = moment().set({'year': 2023, 'month': 7, 'day': 31})
+      const date = moment('2023-08-31', 'YYYY-MM-DD')
 
-      console.log(date)
       expect(operationsForDay(dataset, date)).toStrictEqual(expected)
     });
   })
