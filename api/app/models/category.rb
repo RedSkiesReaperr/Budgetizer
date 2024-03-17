4d682eb39326bfb929d6a5e9fdf546dc4179a83e
RedSkiesReaperr <theopouletsouchard@gmail.com>
2024-03-17 14:18:24 +0100

    Authorize uppercase letters for Category.color

--- api/app/models/category.rb
+++ api/app/models/category.rb
@@ -8,7 +8,7 @@ class Category < ApplicationRecord
 
   validates :key, :color, :icon, presence: true
   validates :key, format: { with: /\A[a-z][_a-z]+[a-z]\z/ }
-  validates :color, format: { with: /\A#(\d|[a-z]){6}\z/ }
+  validates :color, format: { with: /\A#(\d|[a-zA-Z]){6}\z/ }
   validates :icon, format: { with: /\Amdi-[-|a-z0-9]+[a-z0-9]\z/ }
   validates :key, uniqueness: { scope: :user_id }
 end
